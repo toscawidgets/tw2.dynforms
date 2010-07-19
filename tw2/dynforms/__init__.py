06cd4e346bd508de39b4a6177095579d33c3336a
paj <none@none>
2010-07-19 23:48:45 +0100

    Tweak docstrings

--- tw2/dynforms/__init__.py
+++ tw2/dynforms/__init__.py
@@ -1,9 +1,7 @@
 """
 tw2.dynforms is a library of widgets for building dynamic forms. The widgets
 use a combination of client and server logic to allow developers to utilise
-powerful functionality with ease. To get a feel for what's available, you can
-see demos of the available widgets in the widget browser. There is also a
-more detailed demo application in the source distribution.
+powerful functionality with ease.
 """
 
 from widgets import (
