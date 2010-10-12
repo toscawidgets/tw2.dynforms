444b52b2e9868c7ce55da8d35ae5edc24e0e10a5
paj <none@none>
2010-10-12 17:24:11 +0100

    Correct version number

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='tw2.dynforms',
-    version='2.0a1',
+    version='2.0a2',
     description="Dynamic widgets with JavaScript for ToscaWidgets 2",
     long_description = open('README.txt').read().split('\n\n', 1)[1],
     author='Paul Johnston & Contributors',
     author_email='paj@pajhome.org.uk',
     url = "http://toscawidgets.org/documentation/tw2.core/",
     install_requires=[
-        "tw2.forms>=2.0b5",
+        "tw2.forms>=2.0b4",
         "Genshi",
         ],
     packages=find_packages(exclude=['ez_setup', 'tests']),
