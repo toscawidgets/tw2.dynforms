2a42331eda5d1012588255d45e0c86c751618c9b
Ralph Bean <rbean@redhat.com>
2012-05-20 16:28:47 -0400

    PEP8 in samples.py.

--- tw2/dynforms/samples.py
+++ tw2/dynforms/samples.py
@@ -1,18 +1,26 @@
-import tw2.forms as twf, tw2.dynforms as twd
+import tw2.forms as twf
+import tw2.dynforms as twd
+
 
 class DemoGrowingGridLayout(twd.GrowingGridLayout):
     name = twf.TextField()
     phone_number = twf.TextField()
     personal = twf.CheckBox()
 
+
 class DemoHidingCheckBox(twd.HidingTableLayout):
     demo_for = twd.HidingCheckBox
-    delivery = twd.HidingCheckBox(label_text='Delivery required?', mapping={1:['address']})
+    delivery = twd.HidingCheckBox(
+        label_text='Delivery required?',
+        mapping={1: ['address']})
     address = twf.TextField()
+
 
 class DemoHidingCheckBoxList(twd.HidingTableLayout):
     demo_for = twd.HidingCheckBoxList
-    contact = twd.HidingCheckBoxList(label_text='Contact method', options=('E-mail', 'Phone', 'SMS'),
+    contact = twd.HidingCheckBoxList(
+        label_text='Contact method',
+        options=('E-mail', 'Phone', 'SMS'),
         mapping={
             'E-mail': ['email_address'],
             'Phone': ['phone_number'],
@@ -20,10 +28,13 @@ class DemoHidingCheckBoxList(twd.HidingTableLayout):
         })
     email_address = twf.TextField()
     phone_number = twf.TextField()
+
 
 class DemoHidingRadioButtonList(twd.HidingTableLayout):
     demo_for = twd.HidingRadioButtonList
-    contact = twd.HidingRadioButtonList(label_text='Contact method', options=('E-mail', 'Phone', 'SMS'),
+    contact = twd.HidingRadioButtonList(
+        label_text='Contact method',
+        options=('E-mail', 'Phone', 'SMS'),
         mapping={
             'E-mail': ['email_address'],
             'Phone': ['phone_number'],
@@ -31,10 +42,13 @@ class DemoHidingRadioButtonList(twd.HidingTableLayout):
         })
     email_address = twf.TextField()
     phone_number = twf.TextField()
+
 
 class DemoHidingSingleSelectField(twd.HidingTableLayout):
     demo_for = twd.HidingSingleSelectField
-    contact = twd.HidingSingleSelectField(label_text='Contact method', options=('E-mail', 'Phone', 'SMS'),
+    contact = twd.HidingSingleSelectField(
+        label_text='Contact method',
+        options=('E-mail', 'Phone', 'SMS'),
         mapping={
             'E-mail': ['email_address'],
             'Phone': ['phone_number'],
@@ -43,6 +57,8 @@ class DemoHidingSingleSelectField(twd.HidingTableLayout):
     email_address = twf.TextField()
     phone_number = twf.TextField()
 
+
 class DemoLinkContainer(twd.LinkContainer):
-    child = twf.SingleSelectField(options=('', 'www.google.com','www.yahoo.com'))
+    child = twf.SingleSelectField(
+        options=('', 'www.google.com', 'www.yahoo.com'))
     link = 'http://$'
